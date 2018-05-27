643e2ed7bc1b72db46228ca3cf45b59f62cf7641
xezian <xezian@icloud.com>
2018-05-26 22:30:51 -0700

    audio elements being crudely inserted (not working)

--- client/src/components/Utils/Audio/Recorderator.js
+++ client/src/components/Utils/Audio/Recorderator.js
@@ -5,24 +5,59 @@ import Record from './Record';
 import Recording from './Recording';
 
 class Recorderator extends Component {
-    state = {
+    constructor(props){
+      super(props);
+      this.state = {
+        record: false,
+        blobObject: null,
+        isRecording: false,
         recordingState: null,
+      }
     }
     componentDidMount() {
         this.setState({recordingState:"ready"})
+    }
+
+    startRecording= () => {
+      this.setState({
+        record: true,
+        isRecording: true
+      });
+    }
+  
+    stopRecording= () => {
+      this.setState({
+        record: false,
+        isRecording: false
+      });
+    }
+  
+    onStart=() => {
+      console.log('You can tap into the onStart callback');
+    }
+  
+    onStop= (blobObject) => {
+      this.setState({
+        blobURL : blobObject.blobURL
+      });
     }
     handleRecordingState = () => {
         let rtnVar;
         switch(this.state.recordingState){
         case "ready":
             rtnVar = <Record
-                    handleRecord={this.setRecordingState}
+                        handleRecord={this.setRecordingState}
                     />;
             break;
         case "recording":
-            rtnVar = <Recording
-                    handleRecording={this.setRecordingState}
-                    />;
+            rtnVar =<Recording
+                            audioBitsPerSecond= {128000}
+                            handleRecording={this.setRecordingState}
+                            onStart={this.onStart}
+                            onStop={this.onStop}
+                            startRecording={this.startRecording}
+                            stopRecording={this.stopRecording}
+                        />;
             break;
         case "recorded":
             rtnVar = <Play
