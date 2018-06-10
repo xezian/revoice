4d1100ff88fb6173617506bc7433ac67a642bc5f
xezian <xezian@icloud.com>
2018-06-10 12:52:20 -0700

    is it me

--- controllers/storeThisObj.js
+++ controllers/storeThisObj.js
@@ -2,7 +2,7 @@ module.exports = (tempUrl) => {
     return new Promise((res, rej) => {
         if(tempUrl){
             console.log(tempUrl);
-            res("permaUrl");
+            res("not yet but once stored this will be the permanent url of stored file to access for playback");
         }
     })
 }
