d0ba6eb1e782df48707b9ba9dff2fd7692df0eb9
xezian <xezian@icloud.com>
2018-07-01 20:18:56 -0700

    remembering

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
 const ko = require('nekodb');
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/revoice';
-
+console.log('hello')
+console.log(MONGODB_URI);
 ko.connect({
   client: 'mongodb',
   url: MONGODB_URI,
-  options: { useNewUrlParser: true },
 });
 
 module.exports = {
